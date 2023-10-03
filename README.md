7c59e6ab8188cc6ff953f971bd3951d52364a264
kapza94 <lukakapetanovic@icloud.com>
2023-10-03 16:11:13 +0200

    Added inProgress and Completed statuses.

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 WHat does it need to have.
 
-1. Editable input field for new TODO. 
-2. On click it needs to add the TODO item to the list below. 
-3. Once the TODO is added it needs to be able to remove it from the list. 
-4. Needs to have 3 states. TODO -> In progress -> Completed. 
+1. Editable input field for new TODO. --Check
+2. On click it needs to add the TODO item to the list below. --Check 
+3. Once the TODO is added it needs to be able to remove it from the list. --Check 
+4. Needs to have 3 states. TODO -> In progress -> Completed. --Check
+5. Needs to remove from todo section and add to in progress section.
 5. Need to implement a sidebar. 
 
 Nice to have but not priority. 
--- app.js
+++ app.js
@@ -7,20 +7,51 @@ let preventDefault = document.getElementById('formSubmit').addEventListener('cli
     event.preventDefault()
 });
 let allToDos = document.getElementById('allToDos');
+let allInProgress = document.getElementById('allInProgress')
+let allCompleted = document.getElementById('allCompleted');
 
 let counter = 1;
 const addTODO = (event) => {
     event.preventDefault()
+    if (inputField.value === '') {
+        inputField.classList.add('danger')
+        return
+    } else {
+        inputField.classList.remove('danger')
+    }
     let addP = document.createElement('p');
-    addP.innerHTML = `<div id= ${counter++} class= 'singlePItem'><div>${inputField.value}</div> <button onclick='removeTODO(event)' class='removeSinglePItem'>x</button></div>`;
+    addP.innerHTML =
+        `<div id= ${counter++} class= 'singlePItem'>
+        <div>${inputField.value}</div> 
+        <div class= 'buttonContainer'> 
+        <button onclick='deleteTODO(event)' class='deleteSinglePItem'>delete </button>
+        <button onclick='inProgressTODO(event)' class='inProgressItem'>In Progress </button>
+        <button onclick='completedTODO(event)' class='completedItem'>Complete </button>
+        </div>
+        </div>
+        </div>`;
     allToDos.append(addP)
-
-    console.log(counter)
+    inputField.value = '';
 }
 
-const removeTODO = (event) => {
+const deleteTODO = (event) => {
     event.target.parentElement.parentElement.remove()
 }
+
+const inProgressTODO = (event) => {
+    event.target.parentElement.parentElement.parentElement.remove();
+    allInProgress.append(event.target.parentElement.parentElement);
+    console.log(allInProgress);
+}
+
+
+const completedTODO = (event) => {
+    event.target.parentElement.parentElement.remove();
+    allCompleted.append(event.target.parentElement.parentElement);
+    console.log(allCompleted);
+}
+
+//Item state issues. when i add an item. i cx
 
 submitButton.addEventListener('click', addTODO)
 
